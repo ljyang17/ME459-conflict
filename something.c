cc3e7357ea5c77abe00f2fc6ab4a1770914d9811
Lijing Yang <ljyang1717@gmail.com>
2020-10-29 15:34:06 -0500

    Changes to print and add sum

--- something.c
+++ something.c
@@ -3,12 +3,18 @@
 #define N 8
 
 int main(int argc, char* argv[]) {
-    printf("Hello world!\n");
+    printf("Hello World!\n");
 
     for (size_t i = 0; i < N; i++) {
-        printf("%zu ", i);
+        printf("%zu ", i+1);
     }
     printf("\n");
+
+    size_t sum = 0;
+    for (size_t i = 0; i < N; i++) {
+        sum += i;
+    }
+    printf("sum: %zu\n", sum);
 
     return 0;
 }
