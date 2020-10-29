a6eca330dd612c424058ec713b3c10765147afa4
Lijing Yang <ljyang1717@gmail.com>
2020-10-29 15:39:52 -0500

    Add a different sum

--- something.c
+++ something.c
@@ -6,9 +6,21 @@ int main(int argc, char* argv[]) {
     printf("Hello world!\n");
 
     for (size_t i = 0; i < N; i++) {
-        printf("%zu ", i);
+        printf("%zu ", i+2);
     }
     printf("\n");
+
+    int acc = 0;
+    for (int i = 0; i < N; i++) {
+        if (i % 2) {
+            acc += i;
+        }
+        else {
+            acc -= i;
+        }
+    }
+
+    printf("acc: %d\n", acc);
 
     return 0;
 }
